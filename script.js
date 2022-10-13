4c857d4d948ba67489878723ebd9b1204bb93553
karthiksanthamoorthy <karthiksanthamoorthy713@gmail.com>
2022-10-13 16:16:55 +0530

    elementbyid

---
+++ script.js
@@ -0,0 +1,9 @@
+ 
+function foo(){
+  var fname=document.getElementById("Firstname").value;
+  var mnane=document.getElementById("Middlename").value;
+  var lname=document.getElementById("Lastname").value;
+  var ename=document.getElementById("email").value;
+  var pname=document.getElementById("Password").value;
+  console.log(fname,mnane,lname,ename,pname);
+}
